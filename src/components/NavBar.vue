620fc294a9991d83912ed34ea160dd4fe100fb32
Alan <aalan.lima@live.com>
2023-04-18 16:26:59 -0300

    Finished NavBar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,70 @@
 <template>
 
-    <nav>     
-        <RouterLink to="/">
-            <img src="" id="logo">
-        </RouterLink>   
-        <RouterLink to="/"> Home </RouterLink>
-        <RouterLink to="/pedidos"> Pedidos </RouterLink>
-    </nav>
+	<nav>     
+		<RouterLink class="logo-burguer" to="/">
+			<img :src="logo" :alt="alt" id="logo">
+			<h1>Make Your Burguer</h1>
+		</RouterLink>   
+		<RouterLink to="/"> Home </RouterLink>
+		<RouterLink to="/pedidos"> Pedidos </RouterLink>
+	</nav>
 
 </template>
 
 <script>
 
-    export default {
-        name: 'NavBar'
-    }
+	export default {
+		name: 'NavBar',
+		props: ["logo", "alt"]
+	}
 
 </script>
+
+<style scoped>
+
+	nav{
+		background-color: #222;
+		border-bottom: 4px solid #111;
+		padding: 15px 50px;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		font-size: 40px;
+	}
+
+	.logo-burguer{
+		box-sizing: border-box;
+		margin: auto;
+		margin-left: 0;
+		display: flex;
+		align-items: center;
+		background-color: #ffffff25;
+		padding: 10px;
+		border-radius: 20px;
+	}
+
+	nav img{
+		width: 80px;
+		height: 80px;
+	}
+
+	nav a {
+		color: #FCBA03;
+		text-decoration: none;
+		margin: 12px;
+		transition: .5s;
+	}
+
+	nav a:hover{
+		color: #fff;
+	}
+
+	h1{
+		color: #FCBA03;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		margin: 0;
+		margin-left: 15px;
+		font-size: 30px;
+	}
+
+</style>
